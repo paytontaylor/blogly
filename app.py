6e3433e0ebff699d3206a107659e7d61055a7d2c
payton <payton@paytontaylor.net>
2022-11-22 13:04:12 -0600

    add majority of part 2 files

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, session, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.app_context().push()
@@ -17,20 +17,24 @@ db.create_all()
 
 @app.route('/')
 def to_users():
+    """ Redirects to Show All Users """
     return redirect('/users')
 
 @app.route('/users')
 def show_all_users():
+    """ Shows All Users """
     users = User.query.all()
     return render_template('show_users.html', users=users)
 
 
 @app.route('/users/new')
 def show_form():
+    """ Displays New User Form  """
     return render_template('add_user_form.html')
 
 @app.route('/users/new', methods=['POST'])
 def add_user():
+    """ Adds New User to db """
     firstName = request.form['first-name']
     lastName = request.form['last-name']
     url = request.form['url']
@@ -43,16 +47,20 @@ def add_user():
 
 @app.route('/users/<int:uid>')
 def show_user(uid):
+    """ Displays User via User ID set in db """
     user = User.query.get(uid)
-    return render_template('show_user.html', user=user, uid=uid)
+    posts = Post.query.all()
+    return render_template('show_user.html', user=user, uid=uid, posts=posts)
 
 @app.route('/users/<int:uid>/edit')
 def edit_user(uid):
+    """ Displays Edit User Form """
     user = User.query.get(uid)
     return render_template('edit_user_form.html', user=user, uid=uid)
 
 @app.route('/users/<int:uid>/edit', methods=['POST'])
 def edit_user_post(uid):
+    """ Edits and saves an individual user """
     user = User.query.get(uid)
     firstName = request.form['first-name']
     lastName = request.form['last-name']
@@ -66,7 +74,32 @@ def edit_user_post(uid):
 
 @app.route('/users/<int:uid>/delete')
 def delete_user(uid):
+    """ Deletes User """
     user = User.query.get(uid)
     db.session.delete(user)
     db.session.commit()
     return redirect('/users')
+
+@app.route('/users/<int:uid>/posts/new')
+def get_post_form(uid):
+    """ Shows form for new post """
+    user = User.query.get(uid)
+    return render_template('new_post.html', user=user, uid=uid)
+
+@app.route('/users/<int:uid>/posts/new', methods=['POST'])
+def add_post(uid):
+    """ Adds a new post to the user's detail page """
+    title = request.form['title']
+    content = request.form['content']
+    
+    new_user = Post(title=title,content=content)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(f'/users/{uid}')
+
+@app.route('/posts/<int:pid>')
+def show_post(pid):
+    post = Post.query.get(pid)
+    return render_template('show_post.html',post=post)
+
+
--- models.py
+++ models.py
@@ -13,3 +13,18 @@ class User(db.Model):
     first_name = db.Column(db.String(30), nullable = False)
     last_name = db.Column(db.String(30), nullable = False)
     url = db.Column(db.String(500), nullable = True)
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    title = db.Column(db.String(50), nullable = False)
+    content = db.Column(db.String(200), nullable = False)
+    created_at = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    user = db.relationship('User', backref='Post')
